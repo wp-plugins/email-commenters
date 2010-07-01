757029e04af2e30d141a713faaac339a33419d65
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-01 09:19:59 +0000

    * Added e-mail link on edit-comments page, which will send email with subject and link to the post that was commented on. * Tiny code clean ups. * Namespaced functions with yoast_ to prevent function name collisions.
    git-svn-id: https://plugins.svn.wordpress.org/email-commenters/trunk@259260 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- email-commenters.php
+++ email-commenters.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Email commenters
-Version: 1.0
+Version: 1.1
 Plugin URI: http://yoast.com/wordpress/email-commenters/
 Description: Gives a simple mailto: link below a post for logged in admins, to email all commenters on a post.
 Author: Joost de Valk
 Author URI: http://yoast.com/
 */
 
-function add_mailto_commenters_link($content) {
+function yoast_add_mailto_commenters_link( $content ) {
 	if (is_single() && current_user_can('edit_users')) {
 		global $wpdb, $post;
 		$adminemail = get_bloginfo('admin_email');
@@ -19,7 +19,7 @@ function add_mailto_commenters_link($content) {
 		if (count($results) == 0)  {
 			return $content;
 		}
-		$message = rawurlencode("Hi,\n\nI'm emailing you because you commented on my post \"".get_the_title()."\" - ".get_permalink()." .\n\nI wanted to let you know that ");
+		$message = rawurlencode("Hi,\n\nI'm emailing you because you commented on my post \"".get_the_title()."\" - ".get_permalink()." .\n");
 		
 		$output = '<div class="alignright"><p><a href="mailto:'.$adminemail.'?bcc=';
 		foreach ($results as $comment) {
@@ -28,12 +28,21 @@ function add_mailto_commenters_link($content) {
 		}
 		$output .= '&amp;subject=RE: '.get_the_title();
 		$output .= '&amp;body='.$message;
-		$output .= '"><img src="'.plugin_dir_url(__FILE__).'/email-commenters.png" alt="Mail the commenters on this post" title="Mail the commenters on this post"/></a></p></div>';
+		$output .= '"><img src="'.plugin_dir_url(__FILE__).'/email-commenters.png"/></a></p></div>';
 		return $output.$content;
 	}
 	return $content;
 }
+add_filter('the_content','yoast_add_mailto_commenters_link');
 
-add_filter('the_content','add_mailto_commenters_link');
-
+function yoast_add_mailto_action_row( $actions ) {
+	global $comment;
+	$link = 'mailto:'
+				.rawurlencode($comment->comment_author.' <'.$comment->comment_author_email.'>')
+				.'?subject='.rawurlencode('RE: '.$comment->post_title)
+				.'&amp;body='.rawurlencode("Hi,\n\nI'm emailing you because you commented on my post \"".$comment->post_title."\" - ".get_permalink($comment->comment_post_ID)." .\n");
+	$actions['Mailto'] = '<a href="'.$link.'">E-Mail</a>';
+	return $actions;
+}
+add_filter('comment_row_actions','yoast_add_mailto_action_row');
 ?>
--- readme.txt
+++ readme.txt
@@ -4,18 +4,23 @@ Donate link: http://yoast.com/donate/
 Tags: SEO, permalinks
 Requires at least: 2.8
 Tested up to: 3.0
-stable tag: 1.0
+stable tag: 1.1
 
-Gives a simple mailto: link on top of a post for logged in admins, to email all commenters on a post.
+Gives a simple mailto: link on top of a post for logged in admins, to email all commenters on a post, and adds an email link in the edit comments page, to easily email a commenter with a link to the post he commented on.
 
 == Description ==
-Sometimes you want an easy way to e-mail all the people who commented on a certain post. Up till now that took a bit of work, now it's as easy as installing this plugin, and clicking the link you'll get on the top of each post labeled "Mail the commenters on this post"!
+Sometimes you want an easy way to e-mail all the people who commented on a certain post. Up till now that took a bit of work, now it's as easy as installing this plugin, and clicking the link you'll get on the top of each post labeled "Mail the commenters on this post". You can also do the same from the edit comments page for single commenters.
 
 * [Email Commenters](http://yoast.com/wordpress/email-commenters/).
 * Read more about [WordPress SEO](http://yoast.com/articles/wordpress-seo/) so you can get the most out of this plugin.
 * Check out the other [Wordpress plugins](http://yoast.com/wordpress/) by the same author.
 
 == Changelog ==
+
+= 1.1 =
+* Added e-mail link on edit-comments page, which will send email with subject and link to the post that was commented on.
+* Tiny code clean ups.
+* Namespaced functions with yoast_ to prevent function name collisions.
 
 = 1.0 =
 * Changed handling of email.
