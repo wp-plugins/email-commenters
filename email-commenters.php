cdefef41a33b25d951b6d333bbb13b45b41c664e
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-29 20:01:25 +0000

    Update to 1.2, see changelog.
    git-svn-id: https://plugins.svn.wordpress.org/email-commenters/tags/1.2@497010 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- email-commenters.php
+++ email-commenters.php
@@ -1,48 +1,57 @@
 <?php
 /*
 Plugin Name: Email commenters
-Version: 1.1
+Version: 1.2
 Plugin URI: http://yoast.com/wordpress/email-commenters/
-Description: Gives a simple mailto: link below a post for logged in admins, to email all commenters on a post.
+Description: Gives a simple mailto: link below in the toolbar to email all the commenters on a post.
 Author: Joost de Valk
 Author URI: http://yoast.com/
 */
 
-function yoast_add_mailto_commenters_link( $content ) {
-	if (is_single() && current_user_can('edit_users')) {
-		global $wpdb, $post;
-		$adminemail = get_bloginfo('admin_email');
-		$query 		= "SELECT DISTINCT comment_author, comment_author_email FROM $wpdb->comments "
-					 ."WHERE comment_type = '' AND comment_approved = '1' AND comment_post_ID = ".$post->ID;
-		$results 	= $wpdb->get_results($query);
-		
-		if (count($results) == 0)  {
-			return $content;
-		}
-		$message = rawurlencode("Hi,\n\nI'm emailing you because you commented on my post \"".get_the_title()."\" - ".get_permalink()." .\n");
-		
-		$output = '<div class="alignright"><p><a href="mailto:'.$adminemail.'?bcc=';
-		foreach ($results as $comment) {
-			if ($comment->comment_author_email != $adminemail)
-				$output .= rawurlencode($comment->comment_author." <".$comment->comment_author_email.">,");
-		}
-		$output .= '&amp;subject=RE: '.get_the_title();
-		$output .= '&amp;body='.$message;
-		$output .= '"><img src="'.plugin_dir_url(__FILE__).'/email-commenters.png"/></a></p></div>';
-		return $output.$content;
-	}
-	return $content;
-}
-add_filter('the_content','yoast_add_mailto_commenters_link');
-
 function yoast_add_mailto_action_row( $actions ) {
 	global $comment;
+	
+	if ( $comment->comment_type != '' )
+		return $actions;
+		
+	$post = get_post( $comment->comment_post_id );
+	
+	$name = explode( " ", $comment->comment_author );
+	$firstname = $name[0];
+	
 	$link = 'mailto:'
-				.rawurlencode($comment->comment_author.' <'.$comment->comment_author_email.'>')
+				.rawurlencode( $comment->comment_author.' <'.$comment->comment_author_email.'>' )
 				.'?subject='.rawurlencode('RE: '.$comment->post_title)
-				.'&amp;body='.rawurlencode("Hi,\n\nI'm emailing you because you commented on my post \"".$comment->post_title."\" - ".get_permalink($comment->comment_post_ID)." .\n");
+				.'&amp;body='.rawurlencode("Hi ".$firstname.",\n\nI'm emailing you because you commented on my post \"".$post->post_title."\" - ".get_permalink($comment->comment_post_ID)." .\n");
 	$actions['Mailto'] = '<a href="'.$link.'">E-Mail</a>';
 	return $actions;
 }
-add_filter('comment_row_actions','yoast_add_mailto_action_row');
-?>
+add_filter( 'comment_row_actions','yoast_add_mailto_action_row' );
+
+function yoast_admin_bar_comment_link() {
+	global $wp_admin_bar, $wpdb, $post;
+
+	if ( is_singular() && current_user_can('edit_users') && $post->comment_count > 0 ) {
+		
+		$adminemail = get_bloginfo('admin_email');
+		$query 		= "SELECT DISTINCT comment_author_email, comment_author FROM $wpdb->comments "
+					 ."WHERE comment_type = '' AND comment_approved = '1' AND comment_post_ID = ".$post->ID;
+		$results 	= $wpdb->get_results($query);
+		
+		if ( count($results) == 0 ) 
+			return;
+
+		$message = rawurlencode("Hi,\n\nI'm emailing you because you commented on my post \"".get_the_title()."\" - ".get_permalink()." .\n");
+		
+		$url = 'mailto:'.$adminemail.'?bcc=';
+		foreach ($results as $comment) {
+			if ($comment->comment_author_email != $adminemail)
+				$url .= rawurlencode($comment->comment_author." <".$comment->comment_author_email.">,");
+		}
+		$url .= '&amp;subject=RE: '.get_the_title();
+		$url .= '&amp;body='.$message;
+		
+		$wp_admin_bar->add_menu( array( 'id' => 'email-commenters', 'title' => '<span style="margin-top:5px;position:relative;float:left;width:16px;height:16px;background-image:url('.plugin_dir_url(__FILE__).'/email-commenters.png)"></span>', 'href' => $url ) );
+	}
+}
+add_action( 'admin_bar_menu', 'yoast_admin_bar_comment_link', 65 );
--- readme.txt
+++ readme.txt
@@ -1,46 +1,66 @@
 === Email Commenters ===
 Contributors: joostdevalk
 Donate link: http://yoast.com/donate/
-Tags: SEO, permalinks
-Requires at least: 2.8
-Tested up to: 3.0
-stable tag: 1.1
+Tags: mail, email, comments, commenters, comment, response, interaction
+Requires at least: 3.0
+Tested up to: 3.3
+stable tag: 1.2
 
-Gives a simple mailto: link on top of a post for logged in admins, to email all commenters on a post, and adds an email link in the edit comments page, to easily email a commenter with a link to the post he commented on.
+Adds a mail icon in the toolbar that allows emailing everyone who commented on a post.
 
 == Description ==
-Sometimes you want an easy way to e-mail all the people who commented on a certain post. Up till now that took a bit of work, now it's as easy as installing this plugin, and clicking the link you'll get on the top of each post labeled "Mail the commenters on this post". You can also do the same from the edit comments page for single commenters.
+Sometimes you want an easy way to e-mail all the people who commented on a certain post. Up till now that took a bit of work, now it's as easy as installing this plugin, and clicking the mail icon you'll get in the toolbar. You can also do the same from the edit comments page for single commenters.
+
+Both of these functions will allow for far easier interaction with your commenters.
 
 * [Email Commenters](http://yoast.com/wordpress/email-commenters/).
 * Read more about [WordPress SEO](http://yoast.com/articles/wordpress-seo/) so you can get the most out of this plugin.
 * Check out the other [Wordpress plugins](http://yoast.com/wordpress/) by the same author.
 
+* Header image, [conceptual incoming email](http://www.shutterstock.com/pic-72478249/stock-photo-conceptual-illustration-for-incoming-electronic-mail.html) from Shutterstock.
+
 == Changelog ==
 
+= 1.2 =
+
+* Added support for other post types.
+* Moved the mail icon to the toolbar.
+* Improved the email link in the admin interface, it now only shows for normal comments and has the commenters firstname in the message already.
+
 = 1.1 =
+
 * Added e-mail link on edit-comments page, which will send email with subject and link to the post that was commented on.
 * Tiny code clean ups.
 * Namespaced functions with yoast_ to prevent function name collisions.
 
 = 1.0 =
+
 * Changed handling of email.
 * Moved button to top right of post page.
 * Added button to plugin.
 
 = 0.2 =
+
 * Initial release.
+
+== Screenshots ==
+
+1. Screenshot of the email icon that will appear on every post (or other post type) that has comments.
+2. Screenshot of the email link that will be added to comments in the admin interface.
 
 == Installation ==
 
-Installation:
+Install through backend:
+
+1. Search for email commenters.
+1. Click install.
+1. Click activate.
+1. Look at your posts, you should have a new email icon in the toolbar.
+
+Install manually:
 
 1. Unzip the `email-commenters.zip` file. 
 1. Upload the the `email-commenters` folder (not just the files in it!) to your `wp-contents/plugins` folder. 
-
-Activate:
-
 1. In your WordPress administration, go to the Plugins page
 1. Activate the Email Commenters plugin.
-1. Look at your posts, you should have a new icon there.
-
-If you find any bugs or have any ideas, please mail me.
+1. Look at your posts, you should have a new email icon in the toolbar.
